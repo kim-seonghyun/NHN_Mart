90542274e6f2d61a9aba4db8252ba994ef9ba002
김성현 <aruesin2@gmail.com>
2023-09-01 14:40:18 +0900

    [Fix] FoodStand add 메서드 구현 완료

--- src/main/java/com/nhnacademy/mart/FoodStand.java
+++ src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -7,6 +7,9 @@ public class FoodStand {
     private final ArrayList<Food> foods = new ArrayList<>();
 
     // TODO add 메서드 구현
-
+    public void add(Food food){
+        foods.add(food);
+    }
     // TODO 장바구니에 담은 Food 삭제 구현
+
 }
