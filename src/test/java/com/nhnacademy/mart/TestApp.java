592823f0e98b3f85ba9213d3905d76ce28fc0ede
김성현 <aruesin2@gmail.com>
2023-09-01 21:46:41 +0900

    [Fix] FoodStand delete 메서드 구현, add Test Sample

--- src/test/java/com/nhnacademy/mart/TestApp.java
+++ src/test/java/com/nhnacademy/mart/TestApp.java
@@ -2,6 +2,7 @@ package com.nhnacademy.mart;
 
 
 import jdk.jfr.Description;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +27,30 @@ public class TestApp {
         foodStand.delete(new BuyList.Item("apple", 3));
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            foodStand.delete(new BuyList.Item("apple", 1));
+        });
+    }
+
+    @Test
+    @Description("FoodStand 더 많은양 주문 테스트")
+    public void testDeleteOverflow(){
+        FoodStand foodStand = new FoodStand();
+        foodStand.add(new Food("apple", 200));
+        foodStand.add(new Food("apple", 200));
+        foodStand.add(new Food("apple", 200));
+
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            foodStand.delete(new BuyList.Item("apple", 4));
+        });
+    }
+
+    @Test
+    @Description("FoodStand 없는 음식 주문 테스트")
+    public void testDeleteUnderflow(){
+        FoodStand foodStand = new FoodStand();
+
+        Assertions.assertThrows(IllegalArgumentException.class , ()->{
             foodStand.delete(new BuyList.Item("apple", 1));
         });
     }
